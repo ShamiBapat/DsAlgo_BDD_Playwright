d190261e4f7e8741d16bfecadc220c859facfd03
Isha Mahajan <145062247+mahaisha@users.noreply.github.com>
2024-06-10 14:10:28 -0500

    Update pages.js

--- tests/fixtures/pages.js
+++ tests/fixtures/pages.js
@@ -6,7 +6,7 @@ export * from '../pages/GenericPageObj';
 export * from '../pages/QueuePageObj';
 export * from '../pages/ArrayPageObj';
 export * from '../pages/TreePageObj';
-export * from '../pages/LinkedListObj.mjs'
+export * from '../pages/LinkedListObj';
 export * from '../pages/GraphPageObj';
 export * from '../pages/stackPageObj';
 
